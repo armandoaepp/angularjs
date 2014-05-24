63c95d1be7bbfce45a6497acd8d568c62562c3cb
Armando Enrique Pisfil Puemape <armandoaepp@gmail.com>
2014-05-24 12:58:52 -0500

    ordenados por carpetas

--- js/formulario.js
+++
@@ -1,8 +0,0 @@
-function Controlador($scope, $log) {
-
-  $scope.registrar = function(usuario) {
-    $log.info('Usuario: ');
-    $log.info(usuario);
-    $log.info('fue registrado correctamente!');
-  };
-};
